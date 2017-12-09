4d98cb7de97cbf815908f33608334307b91b8ab1
Evan Carroll <me@evancarroll.com>
2017-12-08 18:19:30 -0600

    updated header file

--- src/bignum.h
+++ src/bignum.h
@@ -15,7 +15,8 @@ BIGNUM * bytea_to_bignum(bytea *raw);
 bytea * bignum_to_bytea(BIGNUM *bn);
 
 // big numbers
-Datum pgx_bignum_in(PG_FUNCTION_ARGS);
+Datum pgx_bignum_in_hex(PG_FUNCTION_ARGS);
+Datum pgx_bignum_in_dec(PG_FUNCTION_ARGS);
 Datum pgx_bignum_out(PG_FUNCTION_ARGS);
 
 Datum BnGetDatum(BIGNUM *bn);
